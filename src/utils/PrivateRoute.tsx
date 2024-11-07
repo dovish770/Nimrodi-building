b23e13ec2649e9f5efbc16f53cd371f10c03e875
dovi stein <dovish@gmail.com>
2024-11-07 12:10:54 +0200

    finished PrivateRoutes

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,13 +1,25 @@
 import { ReactNode, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
+import { RootState } from "../store/store"; 
+import Forbidden from '../pages/Forbidden/Forbidden'
 
-interface IPrivateRoute{
-    component: ReactNode,
+interface IPrivateRoute {
+    component: ReactNode;
 }
 
-const PrivateRoute = ({ component}:IPrivateRoute) => {
-   
+const PrivateRoute = ({ component }: IPrivateRoute) => {
+    const { floor } = useParams();
+    const floorAccessStatus = useSelector((state: RootState) => state.floorAccess.floorAccessStatus);
+
+    const [isAccessGranted, setIsAccessGranted] = useState(false);
+
+    useEffect(() => {
+        const floorIndex = Number(floor) - 1;  
+        setIsAccessGranted(floorAccessStatus[floorIndex]);
+    }, [floor, floorAccessStatus]);
+
+    return isAccessGranted ? <>{component}</> : <Forbidden />;
 };
 
-export default PrivateRoute
+export default PrivateRoute;
