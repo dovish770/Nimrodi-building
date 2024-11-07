b01d84441f614606ce16705e32c8d0e564e181ee
dovi stein <dovish@gmail.com>
2024-11-07 10:22:47 +0200

    starting with set role on roleReducer file

--- src/store/rolereducer.ts
+++ src/store/rolereducer.ts
@@ -1,1 +1,14 @@
-//FILL HERE 3.2
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = "Unknown Personnel";
+
+const roleSlice = createSlice({
+  name: "role",
+  initialState,
+  reducers: {
+    setRole: (state, action: PayloadAction<string>) => action.payload
+  }
+});
+
+export const { setRole } = roleSlice.actions;
+export default roleSlice.reducer;
