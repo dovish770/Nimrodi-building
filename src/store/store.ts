942039b495ee318b8be58eeb13117f98f2889539
dovi stein <dovish@gmail.com>
2024-11-07 10:36:46 +0200

    finished store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,14 @@
-// FILL HERE 3.3
+import { configureStore } from "@reduxjs/toolkit";
+import floorReducer from "./floorreducer";
+import roleReducer from "./Rolereducer"; 
+
+const store = configureStore({
+  reducer: {
+    floorAccess: floorReducer,
+    role: roleReducer, 
+  }
+});
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
