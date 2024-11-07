535c8fa65abb78e25ed5fd20d3ce19ca5191f660
dovi stein <dovish@gmail.com>
2024-11-07 11:23:24 +0200

    finished basic routes for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Reception from "./pages/Reception/Reception";
 import Floor from "./pages/Floor/Floor";
 import PrivateRoute from "./utils/PrivateRoute";
 import Layout from "./components/Layout/Layout";
 import Forbidden from "./pages/Forbidden/Forbidden";
 
-const router = createBrowserRouter([
- //FILL HERE
-]);
-
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-      
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Reception />} />
+          <Route path="/floor/:index" element={<Floor />} />
+          <Route path="/forbidden" element={<Forbidden />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
