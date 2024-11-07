1aeb955f62dffaed1416faa91f06ce01cc3b2289
dovi stein <dovish@gmail.com>
2024-11-07 10:16:09 +0200

    supposebily finished floorReducer

--- src/store/floorreducer.ts
+++ src/store/floorreducer.ts
@@ -4,19 +4,24 @@ const initialState = {
   floorAccessStatus: [false, false, false, false, false]
 };
 
+
+function toggleFloorAccess(floorAccessStatus: boolean[], index: number): boolean[] {
+  if (index >= 0 && index < floorAccessStatus.length) {
+    floorAccessStatus[index] = !floorAccessStatus[index];
+  }
+  return [...floorAccessStatus]; 
+}
+
 const floorSlice = createSlice({
   name: "floorAccessStatus",
   initialState,
   reducers: {
     changeAccess: (state, action: PayloadAction<number>) => {
       const index = action.payload;
-      if (index >= 0 && index < state.floorAccessStatus.length) {
-        state.floorAccessStatus[index] = !state.floorAccessStatus[index];
-      }
+      state.floorAccessStatus = toggleFloorAccess(state.floorAccessStatus, index);
     }
   }
 });
-
 
 export const { changeAccess } = floorSlice.actions;
 export default floorSlice.reducer;
